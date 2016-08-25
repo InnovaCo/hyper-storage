d0097619492ce8722c3687a67ba27df24ff37c15
Maga Abdurakhmanov <maqdev@gmail.com>
2016-08-25 12:25:06 +0200

    * working on indexes (sort/key fields extraction)

---
+++ src/main/scala/eu/inn/hyperstorage/indexing/FieldFiltersExtractor.scala
@@ -0,0 +1,63 @@
+package eu.inn.hyperstorage.indexing
+
+import eu.inn.hyperstorage.api.HyperStorageIndexSortItem
+import eu.inn.hyperstorage.db._
+import eu.inn.parser.ast.{BinaryOperation, Expression, Identifier}
+import eu.inn.parser.{HEval, HParser}
+
+class FieldFiltersExtractor(sortByFields: Seq[HyperStorageIndexSortItem]) {
+  final val sortByFieldsMap = sortByFields.zipWithIndex.map{
+    case (s,index) ⇒ (parseIdentifier(s.fieldName), (s,index))
+  }.toMap
+  final val compareOps = Set(">", ">=", "<", "<=", "=").map(Identifier(_))
+  final val andOp = parseIdentifier("and")
+
+  def extract(expression: Expression): Seq[FieldFilter] = {
+    expression match {
+      case bop @ BinaryOperation(left: Identifier, op, right) if compareOps.contains(op) && AstComparator.isConstantExpression(right) ⇒
+        fieldFilterSeq(left, op.segments.head, right)
+
+      case bop @ BinaryOperation(left, op, right: Identifier) if compareOps.contains(op) && AstComparator.isConstantExpression(left) ⇒
+        fieldFilterSeq(right, swapOp(op.segments.head), left)
+
+      case bop @ BinaryOperation(left, op, right) if op == andOp ⇒
+        extract(left) ++ extract(right)
+
+      case _ ⇒ Seq.empty
+    }
+  }
+
+  private def fieldFilterSeq(varIdent: Identifier, op: String, constExpr: Expression): Seq[FieldFilter] = {
+    sortByFieldsMap.get(varIdent).map { sf ⇒
+      FieldFilter(
+        IndexLogic.tableFieldName(sf._1, sf._2),
+        new HEval().eval(constExpr),
+        filterOp(op)
+      )
+    }.toSeq
+  }
+
+  private def swapOp(op: String) = {
+    op match {
+      case ">" ⇒ "<"
+      case ">=" ⇒ "<="
+      case "<" ⇒ ">"
+      case "<=" ⇒ ">="
+      case "=" ⇒ "="
+    }
+  }
+
+  private def filterOp(op: String) = {
+    op match {
+      case ">" ⇒ FilterGt
+      case ">=" ⇒ FilterGtEq
+      case "<" ⇒ FilterLt
+      case "<=" ⇒ FilterLtEq
+      case "=" ⇒ FilterEq
+    }
+  }
+
+  private def parseIdentifier(s: String): Identifier = {
+    new HParser(s).Ident.run().get
+  }
+}
--- src/test/scala/FilterFieldsExtractorTest.scala
+++ src/test/scala/FilterFieldsExtractorTest.scala
@@ -1,67 +1,12 @@
 import eu.inn.binders.value.{Number, Text}
 import eu.inn.hyperstorage.api.{HyperStorageIndexSortFieldType, HyperStorageIndexSortItem, HyperStorageIndexSortOrder}
 import eu.inn.hyperstorage.db._
-import eu.inn.hyperstorage.indexing.{AstComparation, AstComparator, IndexLogic}
+import eu.inn.hyperstorage.indexing.{AstComparation, AstComparator, FieldFiltersExtractor, IndexLogic}
 import eu.inn.parser.{HEval, HParser}
 import eu.inn.parser.ast.{BinaryOperation, Expression, Identifier}
 import org.scalatest.{FreeSpec, Matchers}
 
-class FieldFiltersExtractor(sortByFields: Seq[HyperStorageIndexSortItem]) {
-  final val sortByFieldsMap = sortByFields.zipWithIndex.map{
-    case (s,index) ⇒ (parseIdentifier(s.fieldName), (s,index))
-  }.toMap
-  final val compareOps = Set(">", ">=", "<", "<=", "=").map(Identifier(_))
-  final val andOp = parseIdentifier("and")
 
-  def extract(expression: Expression): Seq[FieldFilter] = {
-    expression match {
-      case bop @ BinaryOperation(left: Identifier, op, right) if compareOps.contains(op) && AstComparator.isConstantExpression(right) ⇒
-        fieldFilterSeq(left, op.segments.head, right)
-
-      case bop @ BinaryOperation(left, op, right: Identifier) if compareOps.contains(op) && AstComparator.isConstantExpression(left) ⇒
-        fieldFilterSeq(right, swapOp(op.segments.head), left)
-
-      case bop @ BinaryOperation(left, op, right) if op == andOp ⇒
-        extract(left) ++ extract(right)
-
-      case _ ⇒ Seq.empty
-    }
-  }
-
-  private def fieldFilterSeq(varIdent: Identifier, op: String, constExpr: Expression): Seq[FieldFilter] = {
-    sortByFieldsMap.get(varIdent).map { sf ⇒
-      FieldFilter(
-        IndexLogic.tableFieldName(sf._1, sf._2),
-        new HEval().eval(constExpr),
-        filterOp(op)
-      )
-    }.toSeq
-  }
-
-  private def swapOp(op: String) = {
-    op match {
-      case ">" ⇒ "<"
-      case ">=" ⇒ "<="
-      case "<" ⇒ ">"
-      case "<=" ⇒ ">="
-      case "=" ⇒ "="
-    }
-  }
-
-  private def filterOp(op: String) = {
-    op match {
-      case ">" ⇒ FilterGt
-      case ">=" ⇒ FilterGtEq
-      case "<" ⇒ FilterLt
-      case "<=" ⇒ FilterLtEq
-      case "=" ⇒ FilterEq
-    }
-  }
-
-  private def parseIdentifier(s: String): Identifier = {
-    new HParser(s).Ident.run().get
-  }
-}
 
 class FilterFieldsExtractorTest extends FreeSpec with Matchers {
   "FilterFieldsExtractor" - {
